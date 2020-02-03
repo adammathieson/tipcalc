71543d7ad9dcdc23ad79d2f07b1bcad6122a1e9e
Adam Mathieson <mathieson1222@gmail.com>
2020-02-03 15:26:07 -0800

    basics of styling, like styled components, lib available in docs for full list of avialable properties

--- Hello.js
+++ Hello.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import { View, Text} from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default function Hello() {
     return (
         <View>
-            <Text>Hello from Hello.js</Text>
+            <Text style={styles.hello} >Hello from Hello.js</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    hello: {
+        backgroundColor: '#00FF00',
+        fontSize: 24,
+    }
+})
 
 
